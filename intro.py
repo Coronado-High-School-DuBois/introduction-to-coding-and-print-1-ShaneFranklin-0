477795009761a2b2cebbd757ee3d5f2ec80b5501
ShaneFranklin-0 <shane.franklin@d11.org>
2024-09-24 18:29:58 +0000

    Done

--- intro.py
+++ intro.py
@@ -1,37 +1,50 @@
 # 1. Print "Hello World!" to the console
+print("Hello World!\n")
 
 
 
 # 2. Print "Hello my name is ______!" to the console
+print("Hello my name is ______!\n")
 
 
 
 # 3. Print "I love python!" to the console
+print("I love python!\n")
 
 
 
 # 4. Print your age to the console
-
+print(14)
+print()
 
 
 # 5. Print 5+5
-
+print(5+5)
+print
 
 
 # 6. Print 4*3
-
+print(4*3)
+print()
 
 
 # 7. Print 50 - 40
-
+print(50-40)
+print()
 
 
 # 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
-
+print(100/5)
+print(100//5)
+### The first statement prints the exact result, while the second statement prints the result without the remainder (as an integer rather than a float)
+print()
 
 
 # 9. Print 2**4
-
+print(2**4)
+print()
 
 
 # 10. Print 6*3-4+8//2
+print(6*3-4+8//2)
+print()
